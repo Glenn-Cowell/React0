1b6da5f17ee23067cd9085da9df43e94d17bdac9
Glenn-Cowell <xxe1987@gmail.com>
2015-08-25 17:47:04 +0800

    上課相關文件

---
+++ react1/tutorial1.js
@@ -0,0 +1,88 @@
+﻿
+      var HelloMessage = React.createClass({
+		  render: function() {
+			return <div>Hello {this.props.name}</div>;
+		  }
+		});
+		
+		var TodoList = React.createClass({
+		  render: function() {
+			var createItem = function(itemText, index) {
+			  return <li key={index + itemText}>{itemText}</li>;
+			};
+			return <ul>{this.props.items.map(createItem)}</ul>;
+		  }
+		});
+		var TodoApp = React.createClass({
+		  getInitialState: function() {
+			return {items: [], text: ''};
+		  },
+		  onChange: function(e) {
+			this.setState({text: e.target.value});
+		  },
+		  handleSubmit: function(e) {
+			e.preventDefault();
+			var nextItems = this.state.items.concat([this.state.text]);
+			var nextText = '';
+			this.setState({items: nextItems, text: nextText});
+		  },
+		  render: function() {
+			return (
+			  <div>
+				<h3>TODO</h3>
+				<TodoList items={this.state.items} />
+				<form onSubmit={this.handleSubmit}>
+				  <input onChange={this.onChange} value={this.state.text} />
+				  <button>{'Add #' + (this.state.items.length + 1)}</button>
+				</form>
+			  </div>
+			);
+		  }
+		});
+		
+		
+
+var NotesList = React.createClass({
+  render: function () {
+    var notes = this.props.notepad.notes;
+
+    return (
+      <div className="note-list">
+      {
+        notes.map(function (note) {
+          var title = note.content.substring(0,
+            note.content.indexOf('\n')
+          );
+          title = title || note.content;
+
+          return (
+            <div key={note.id} className="note-summary">
+              {title}
+            </div>
+          );
+        })
+      }
+      </div>
+    );
+  }
+});
+var notepad = {
+  notes: [
+    {
+      id: 1,
+      content: "Hello, world!\nBoring.\nBoring.\nBoring."
+    },
+    {
+      id: 2,
+      content: "React is awesome.\nSeriously, it's the greatest."
+    },
+    {
+      id: 3,
+      content: "Robots are pretty cool.\nRobots are awesome, until they take over."
+    },
+    {
+      id: 4,
+      content: "Monkeys.\nWho doesn't love monkeys?"
+    }
+  ]
+}
---
+++ react4/menu1.asp
@@ -0,0 +1,49 @@
+﻿
+<!--#include virtual="/include/egrpDSN_binary.inc"-->
+<%
+	LVL_CD = Request("LVL_CD")
+	PMENU_ID = Request("MENU_ID")
+	LVL_CD = len(PMENU_ID)+1
+	session.CodePage = 65001
+	response.charset = "utf-8"
+	set Rs = Server.CreateObject("Adodb.Recordset")
+	set RsSub1 = Server.CreateObject("Adodb.Recordset")
+	set RsSub2 = Server.CreateObject("Adodb.Recordset")
+	set RsSub3 = Server.CreateObject("Adodb.Recordset")
+	sSQL = "SELECT a.LVL_CD, a.MENU_ID, a.MENU_CNM, a.PMENU_ID, isnull(b.ASP_ENM,'') as asp_enm  FROM dbo.TRMENU a "
+	sSQL = sSQL & " LEFT OUTER JOIN dbo.TRMENUASP b ON a.MENU_ID = b.MENU_ID and b.MA_SQ = 1 "
+	sSQL = sSQL & " WHERE LEFT(a.menu_id,2)<>'NI' "
+	sSQL = sSQL & " and a.menu_id in (select left(menu_id,len(a.menu_id)) from trmenuasp) "
+	sSQL = sSQL & " and a.LVL_CD = '"&LVL_CD&"' and a.PMENU_ID='"&PMENU_ID&"' ORDER BY a.MENU_ID"
+	Rs.open sSQL,egrpDSN,1,1
+	cnt = 0
+	if rs.eof then
+		response.write "null"
+	else
+		'response.write "{ ""menu"":["
+			response.write "["
+		do until Rs.eof
+			cnt = cnt + 1
+			if cnt > 1 then response.write ","
+			LVL_CD = trim(Rs("LVL_CD"))
+			MENU_ID = trim(Rs("MENU_ID"))
+			MENU_CNM = trim(Rs("MENU_CNM"))
+			PMENU_ID = trim(Rs("PMENU_ID"))
+			asp_enm = trim(Rs("asp_enm"))
+
+			response.write "{"
+
+			response.write """MENU_NO"":"""&MENU_ID&""","
+			response.write """MENU_NM"":"""&MENU_CNM&""","
+
+			response.write """url"":"""&asp_enm&""""
+			response.write "}"
+			rs.movenext
+		loop
+		response.write "]"
+		'response.write "]}"
+	end if
+	rs.close
+	set RsSub1 = nothing
+	set rs = nothing
+%>
